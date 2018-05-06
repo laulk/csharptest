6d7284b1091c5dcfd000f9c12e24102f9f4f311c
Unknown <laulk.054@gmail.com>
2018-05-06 02:48:27 +0100

    txt file reader? didnt work
    checking & comparing list objects, dont know how c# works

---
+++ EmptyClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+/*namespace tutorials
+{
+    class ReadWords
+    {
+       static void Main()
+        {
+            StreamReader file = new StreamReader("words.txt");
+            string line = file.ReadLine();
+            string[] elements = line.Split(' ');
+
+            string theWord = (elements[0]);
+            string ID = (elements[1]);
+            int numberTimesLearned = Convert.ToInt32(elements[2]);
+            bool hasCollected = Convert.ToBoolean(elements[3]);
+
+            foreach (string line1 in elements)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
+*/
--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace tutorials
 {
@@ -10,12 +12,36 @@ namespace tutorials
             {
                 new Word{theWord= "Apple", ID = "WordSticker1", numberOfTimesLearned=0,hasAlreadyCollected=false},
                 new Word{theWord= "Bubble", ID = "WordSticker2", numberOfTimesLearned=0,hasAlreadyCollected=false},
-                new Word{theWord= "Couple", ID = "WordSticker3", numberOfTimesLearned=0,hasAlreadyCollected=false}
+                new Word{theWord= "Couple", ID = "WordSticker3", numberOfTimesLearned=0,hasAlreadyCollected=false},
+                new Word{theWord= "Double", ID = "WordSticker4", numberOfTimesLearned=0,hasAlreadyCollected=false},
+                new Word{theWord= "Epple", ID = "WordSticker5", numberOfTimesLearned=0,hasAlreadyCollected=false}
             };
 
-            foreach (var word in words)
-             Console.WriteLine("Word: {0} ID: {1}, Learned: {2}, Number of Times learned: {3}", word.theWord, word.ID, word.hasAlreadyCollected, word.numberOfTimesLearned);
-            //to check if appended list with multiple data types is accepted
+            List<LearnedLastLesson> learneds = new List<LearnedLastLesson>
+            {
+                new LearnedLastLesson{iD = "WordSticker1"},
+                new LearnedLastLesson{iD = "WordSticker2"},
+                new LearnedLastLesson{iD = "WordSticker3"}
+            };
+
+
+            Console.WriteLine("Lets see your achievement shall we?");
+
+
+
+
+            //var learned = learneds.Where(d => d.iD == "WordSticker3").FirstOrDefault();
+            //if (learned != null) 
+            //{
+            //    var word = words.Where(i => i.ID == iD).FirstOrDefault(); 
+            //    word.hasAlreadyCollected = true; 
+            //}
+            //words.Where(d => d.theWord == "Apple").First().hasAlreadyCollected = true; 
+
+            //Uncomment the following to check if changed data in the list is saved.
+            foreach (var word3 in words)
+             Console.WriteLine("Word: {0} ID: {1}, Learned: {2}, Number of Times learned: {3}", word3.theWord, word3.ID, word3.hasAlreadyCollected, word3.numberOfTimesLearned);
+
 
         }
     }
@@ -28,5 +54,16 @@ namespace tutorials
         public int numberOfTimesLearned { get; set; }
         public bool hasAlreadyCollected { get; set; }
     
+    }
+
+    public class LearnedLastLesson
+    {
+        public string iD { get; set; }
+
+    }
+
+    internal bool DoIDsMatch(List<LearnedLastLesson> learneds, List<Word> words)
+    {
+        return !learneds.Except(words).Any();
     }
 }
